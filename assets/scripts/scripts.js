8198258c1c19483065cff47f6042964f8e6ad2c7
Alex MacArthur <alex.macarthur@daveramsey.com>
2017-11-11 13:59:00 -0600

    Add event tracking for sharing.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,4 +1,12 @@
 document.getElementById('shareFacebook').addEventListener('click', () => {
+
+  if(typeof gtag !== undefined) {
+    gtag('event', 'facebookShare', {
+      'event_category': 'share',
+      'event_label': 'Shared on Facebook'
+    });
+  }
+
   window.open(
     `https://www.facebook.com/sharer.php?u=${window.location.href}`,
     'sharer',
@@ -6,6 +14,14 @@ document.getElementById('shareFacebook').addEventListener('click', () => {
 });
 
 document.getElementById('shareTwitter').addEventListener('click', () => {
+
+  if(typeof gtag !== undefined) {
+    gtag('event', 'twitterShare', {
+      'event_category': 'share',
+      'event_label': 'Shared on Twitter'
+    });
+  }
+
   window.open(
     `http://twitter.com/intent/tweet?text=${encodeURIComponent('Happy Marriage, Isabella! Hope it\'s as weird as you are. ')}${encodeURIComponent(window.location.href)}`,
     'sharer',
