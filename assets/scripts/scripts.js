a9f84356845542c24d325531792f953b0fcd01e2
Alex MacArthur <alex.macarthur@daveramsey.com>
2017-11-10 23:29:39 -0600

    Update social copy.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,13 +1,13 @@
 document.getElementById('shareFacebook').addEventListener('click', () => {
   window.open(
-    `https://www.facebook.com/sharer.php?u=${window.location.href}&t=${encodeURIComponent('Happy Marriage, Taylor & Isabella! Hope it\'s weird.')}`,
+    `https://www.facebook.com/sharer.php?u=${window.location.href}`,
     'sharer',
     'toolbar=0,status=0,width=626,height=436');
 });
 
 document.getElementById('shareTwitter').addEventListener('click', () => {
   window.open(
-    `http://twitter.com/home?status=${encodeURIComponent('Happy Marriage, Taylor & Isabella! Hope it\'s weird. ')}${encodeURIComponent(window.location.href)}`,
+    `http://twitter.com/home?status=${encodeURIComponent('Happy Marriage, Isabella! Hope it\'s as weird as you are. ')}${encodeURIComponent(window.location.href)}`,
     'sharer',
     'toolbar=0,status=0,width=626,height=436');
 });
